53f2ef42fe3f9ce56aeca98964a2a8c91174ff03
Bram Stein <b.l.stein@gmail.com>
2010-02-07 20:18:39 +0100

    Fixed setting values to zero bug (Thanks John Bowers.) Also made the isVisible method use the standard jQuery :visible selector.

--- jquery.sizes.js
+++ jquery.sizes.js
@@ -1,16 +1,18 @@
-
 /*!
- * JSizes - JQuery plugin v0.32
+ * JSizes - JQuery plugin v0.33
  *
  * Licensed under the revised BSD License.
- * Copyright 2008-2009 Bram Stein
+ * Copyright 2008-2010 Bram Stein
  * All rights reserved.
  */
 /*global jQuery*/
 (function ($) {
 	var num = function (value) {
-		return parseInt(value, 10) || 0;
-	};
+			return parseInt(value, 10) || 0;
+		},
+		isNumber = function (value) {
+			return (typeof value === 'number' || value instanceof Number) && !isNaN(value);
+		};
 
 	/**
 	 * Sets or gets the values for min-width, min-height, max-width
@@ -20,10 +22,10 @@
 		$.fn[name + 'Size'] = function (value) {
 			var width, height;
 			if (value) {
-				if (value.width) {
+				if (isNumber(value.width)) {
 					this.css(name + '-width', value.width);
 				}
-				if (value.height) {
+				if (isNumber(value.height)) {
 					this.css(name + '-height', value.height);
 				}
 				return this;
@@ -44,7 +46,7 @@
 	 * Returns whether or not an element is visible.
 	 */
 	$.fn.isVisible = function () {
-		return /*this.css('visibility') !== 'hidden' &&*/ this.css('display') !== 'none';
+		return this.is(':visible');
 	};
 
 	/**
@@ -53,16 +55,16 @@
 	$.each(['border', 'margin', 'padding'], function (i, name) {
 		$.fn[name] = function (value) {
 			if (value) {
-				if (value.top) {
+				if (isNumber(value.top)) {
 					this.css(name + '-top' + (name === 'border' ? '-width' : ''), value.top);
 				}
-				if (value.bottom) {
+				if (isNumber(value.bottom)) {
 					this.css(name + '-bottom' + (name === 'border' ? '-width' : ''), value.bottom);
 				}
-				if (value.left) {
+				if (isNumber(value.left)) {
 					this.css(name + '-left' + (name === 'border' ? '-width' : ''), value.left);
 				}
-				if (value.right) {
+				if (isNumber(value.right)) {
 					this.css(name + '-right' + (name === 'border' ? '-width' : ''), value.right);
 				}
 				return this;
